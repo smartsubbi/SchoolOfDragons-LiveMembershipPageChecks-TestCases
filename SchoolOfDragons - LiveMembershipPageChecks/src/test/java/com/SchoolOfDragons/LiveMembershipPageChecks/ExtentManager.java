1bc97ac2dc1152289798ffe0b24b400d3b8ff3d0
subbu <subbu@DESKTOP-J13P4A7>
2016-05-07 19:41:21 +0530

    First Check in for Live Membership Page Checks

---
+++ SchoolOfDragons - LiveMembershipPageChecks/src/test/java/com/SchoolOfDragons/LiveMembershipPageChecks/ExtentManager.java
@@ -0,0 +1,28 @@
+package com.SchoolOfDragons.LiveMembershipPageChecks;
+
+import java.net.InetAddress;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.relevantcodes.extentreports.ExtentReports;
+
+public class ExtentManager 
+{
+	static DateFormat dateFormat = new SimpleDateFormat("E.MMMM.yyyy_HH.mm.ss[a]");
+	static Date date = new Date();
+	static String finalPath;	
+	
+	public static ExtentReports Instance() throws Throwable 
+	{
+		String userDirectory =  System.getProperty("user.dir");
+		ExtentReports report;
+		InetAddress address = InetAddress.getLocalHost(); 
+		String hostIP = address.getHostAddress();	 	        
+	    String splitDir[] = userDirectory.split(":");				
+		finalPath = "\\\\"+hostIP+splitDir[1]+"\\ExtentReports\\SchoolOfDragonsLive_" +dateFormat.format(date) + ".html";	
+		report = new ExtentReports(finalPath, false);
+		report.config().documentTitle("Automation Report").reportName("Regression");
+		return report;
+	}		
+}
